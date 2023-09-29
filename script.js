8b2c2c18030e44250d1afb3b4b48c8a93a30839f
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2023-09-29 14:07:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 // Define the players array and person object
 const players = ["John", "Bob", "Alice", "Poppy"];
 
@@ -8,18 +6,16 @@ const person = {
   age: 80,
 };
 
+// Create a reference to the players array using the variable team
+const team = players;
+
+// Create a copy of the players array using the variable team1
+const team1 = [...players];
+
+// Create a copy of the person object using the variable cap1
+const cap1 = { ...person };
+
 // Attach the variables to the global window object
-window.players = players;
-window.person = person;
-
-// Define additional variables as needed
-const team = "Sample Team";
-const team1 = "Another Team";
-const cap1 = "Captain";
-
-// Attach the additional variables to the global window object
 window.team = team;
 window.team1 = team1;
 window.cap1 = cap1;
-
-// You can access these variables globally in other scripts or functions
